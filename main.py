bbb1dfe2f520aac9863013defd5b801ee5787abd
PiticarAndreea <126388256+tdo27@users.noreply.github.com>
2026-01-23 00:29:42 +0200

    Remove duplicate header information from main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
-import unicode_tr # daca il folosesti
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -52,3 +52,4 @@ class YTDownloader(App):
 ...             self.label.text = f"Eroare: {str(e)}"
 ... 
 ... if __name__ == '__main__':
+
